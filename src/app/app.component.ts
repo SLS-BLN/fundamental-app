dd34e7503fbde5ebc777f76600bca80d1d53ea5f
Stefan Schlicker <stefanschlicker@posteo.de>
2023-09-12 11:59:02 +0200

    intial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ShellbarUser, ShellbarUserMenu } from '@fundamental-ngx/core/shellbar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'fundamental-app';
+  user: ShellbarUser = {
+    fullName: 'Daniel Richert',
+    colorAccent: 6,
+  };
+
+  userMenu: ShellbarUserMenu[] = [
+    { text: 'Settings', callback: this.settingsCallback },
+    { text: 'Sign Out', callback: this.signOutCallback },
+  ];
+
+  settingsCallback(): void {
+    alert('Settings Clicked');
+  }
+
+  signOutCallback(): void {
+    alert('Sign Out Clicked');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FundamentalNgxCoreModule } from '@fundamental-ngx/core';
+import { ThemingService, ThemingModule } from '@fundamental-ngx/core/theming';
+import { CheckboxModule } from '@fundamental-ngx/core/checkbox';
+import { BreadcrumbModule } from '@fundamental-ngx/core/breadcrumb';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FundamentalNgxCoreModule,
+    ThemingModule.withConfig({
+      defaultTheme: 'sap_horizon',
+      changeThemeOnQueryParamChange: false,
+    }),
+    CheckboxModule,
+    BreadcrumbModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(themingService: ThemingService) {
+    themingService.init();
+  }
+}
